2f4c90d53348e709c90f3f678f58e8fc0748d18f
shivam <shivam@TeamCity>
2026-03-25 11:19:37 +0000

    Versioned settings configuration updated (TeamCity change in 'hello-app' project)

--- .teamcity/patches/buildTypes/hello_app_build.kts
+++ .teamcity/patches/buildTypes/hello_app_build.kts
@@ -17,10 +17,6 @@ changeBuildType(RelativeId("hello_app_build")) {
         script {
             name = "Set Commit ID"
             id = "Set_Commit_ID"
-
-            conditions {
-                doesNotEqual("env.isProdBuild", "Yes")
-            }
             scriptContent = """
                 SHORT_COMMIT_HASH=${'$'}(git rev-parse --short HEAD)
                 echo "##teamcity[setParameter name='env.COMMIT_ID' value='${'$'}SHORT_COMMIT_HASH']"
@@ -30,10 +26,6 @@ changeBuildType(RelativeId("hello_app_build")) {
         dockerCommand {
             name = "dockerbuild"
             id = "dockerbuild"
-
-            conditions {
-                doesNotEqual("env.isProdBuild", "Yes")
-            }
             commandType = build {
                 source = file {
                     path = "Dockerfile"
@@ -50,10 +42,6 @@ changeBuildType(RelativeId("hello_app_build")) {
         dockerCommand {
             name = "docker image push"
             id = "docker_image_push"
-
-            conditions {
-                doesNotEqual("env.isProdBuild", "Yes")
-            }
             commandType = push {
                 namesAndTags = """
                     088332244542.dkr.ecr.ap-south-1.amazonaws.com/non-prod/hello-app:%teamcity.build.branch%_%build.number%
@@ -66,16 +54,8 @@ changeBuildType(RelativeId("hello_app_build")) {
             name = "DeployDev"
             id = "DeployDev"
             type = "octopus.create.release"
-
-            conditions {
-                equals("teamcity.build.branch", "main")
-                doesNotEqual("env.isProdBuild", "Yes")
-            }
-            param("octopus_additionalcommandlinearguments", """--variable="DockerTag=%teamcity.build.branch%_%build.number%"""")
             param("octopus_space_name", "OASIS")
-            param("octopus_waitfordeployments", "true")
             param("octopus_channel_name", "Development")
-            param("octopus_version", "3.0+")
             param("octopus_host", "http://10.29.1.84")
             param("octopus_project_name", "hello-app")
             param("octopus_deployto", "Development")
@@ -86,16 +66,8 @@ changeBuildType(RelativeId("hello_app_build")) {
             name = "DeployStage"
             id = "DeployStage"
             type = "octopus.create.release"
-
-            conditions {
-                equals("teamcity.build.branch", "stage")
-                doesNotEqual("env.isProdBuild", "Yes")
-            }
-            param("octopus_additionalcommandlinearguments", """--variable="DockerTag=%teamcity.build.branch%_%build.number%"""")
             param("octopus_space_name", "OASIS")
-            param("octopus_waitfordeployments", "true")
             param("octopus_channel_name", "Stage")
-            param("octopus_version", "3.0+")
             param("octopus_host", "http://10.29.1.84")
             param("octopus_project_name", "hello-app")
             param("octopus_deployto", "Stage")
@@ -106,16 +78,8 @@ changeBuildType(RelativeId("hello_app_build")) {
             name = "DeployUAT"
             id = "DeployUAT"
             type = "octopus.create.release"
-
-            conditions {
-                equals("teamcity.build.branch", "uat")
-                doesNotEqual("env.isProdBuild", "Yes")
-            }
-            param("octopus_additionalcommandlinearguments", """--variable="DockerTag=%teamcity.build.branch%_%build.number%"""")
             param("octopus_space_name", "OASIS")
-            param("octopus_waitfordeployments", "true")
             param("octopus_channel_name", "Uat")
-            param("octopus_version", "3.0+")
             param("octopus_host", "http://10.29.1.84")
             param("octopus_project_name", "hello-app")
             param("octopus_deployto", "Uat")
@@ -125,11 +89,6 @@ changeBuildType(RelativeId("hello_app_build")) {
         script {
             name = "Retag_UAT_image_for_Prod"
             id = "Retag_release_branch_image_for_Prod"
-
-            conditions {
-                equals("teamcity.build.branch", "uat")
-                equals("env.isProdBuild", "Yes")
-            }
             scriptContent = """
                 docker pull 088332244542.dkr.ecr.ap-south-1.amazonaws.com/non-prod/hello-app:uat_latest
                 docker tag 088332244542.dkr.ecr.ap-south-1.amazonaws.com/non-prod/hello-app:uat_latest 088332244542.dkr.ecr.ap-south-1.amazonaws.com/prod/hello-app:prod_%build.number%
@@ -139,11 +98,6 @@ changeBuildType(RelativeId("hello_app_build")) {
         dockerCommand {
             name = "Push_Prod_image"
             id = "Push_Prod_image"
-
-            conditions {
-                equals("teamcity.build.branch", "uat")
-                equals("env.isProdBuild", "Yes")
-            }
             commandType = push {
                 namesAndTags = """
                     088332244542.dkr.ecr.ap-south-1.amazonaws.com/prod/hello-app:prod_%build.number%
